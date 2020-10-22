147ded6a16b267631d0ac8ca326eaed1190a2d12
Khaildoran <sgermscheid@gmail.com>
2020-10-21 21:15:46 -0500

    guest list, guest form, and dinner supplises done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import Header from '../Header/Header'
+import GuestForm from '../GuestForm/GuestForm'
+import GuestList from '../GuestList/GuestList'
+import DinnerSupplies from '../DinnerSupplies/DinnerSupplies'
 import './App.css';
 
 class App extends Component {
@@ -11,6 +15,7 @@ class App extends Component {
   }
 
   handleChangeFor = (propertyName) => (event) => {
+    console.log('in function');
     this.setState({
       newGuest: {
         ...this.state.newGuest,
@@ -20,8 +25,11 @@ class App extends Component {
   }
 
   handleSubmit = (event) => {
+    console.log('in function');
+    
     event.preventDefault();
     if (this.state.newGuest.name) {
+      console.log('yay');
       this.setState({
         guestList: [...this.state.guestList, this.state.newGuest],
         newGuest: {
@@ -37,77 +45,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>
-          <h1>Prim Proper Props</h1>
-        </header>
+        < Header/>
         <h2>Party Leader</h2>
         {this.state.guestList[0] && <h3>{this.state.guestList[0].name}</h3>}
-        <h2>Add a new guest</h2>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name
-          </label>
-          <input
-            type="text"
-            placeholder="Name"
-            value={this.state.newGuest.name}
-            onChange={this.handleChangeFor('name')}
-          />
-          <div>
-            Would this guest like a kid's meal?
-            <div onChange={this.handleChangeFor('kidsMeal')}>
-              <div>
-                <label>
-                  <input
-                    type="radio"
-                    value="yes"
-                    name="kidsMeal"
-                  />
-                  Yes, this guest would like a Kid's Meal
-                </label>
-              </div>
-              <div>
-                <label>
-                  <input
-                    type="radio"
-                    value="no"
-                    defaultChecked
-                    name="kidsMeal"
-                  />
-                  No, this guest would not like a Kid's Meal
-                </label>
-              </div>
-            </div>
-          </div>
-          <button type="submit">Add Guest</button>
-        </form>
-        <h2>Guest List</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Kid's Meal</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.guestList.map(guest => (
-              <tr key={guest.name}>
-                <td>{guest.name}</td>
-                <td>{guest.kidsMeal}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <h2>Dinner Supplies</h2>
-        <div>
-          Spoons: {this.state.guestList.length * 2}
-        </div>
-        <div>
-          Forks: {this.state.guestList.length * 2}
-        </div>
-        <div>
-          Knives: {this.state.guestList.length * 2}
-        </div>
+        <GuestForm
+        newGuest={this.state.newGuest}
+        handleChangeFor={this.handleChangeFor}
+        handleSubmit={this.handleSubmit}
+        />
+         <GuestList guests={this.state.guestList}/>
+         <DinnerSupplies count={this.state.guestList.length} />
         <footer>
           <h3>Have fun!</h3>
           <p>Don't forget to mind your Ps and Qs!</p>
